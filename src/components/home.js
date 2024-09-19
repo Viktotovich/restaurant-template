21344f62b1e47214325b375f674bd1c896cecc8d
Vladimir Laskin <148013724+Viktotovich@users.noreply.github.com>
2024-09-19 16:06:46 +0400

    "Converted mp4 to gif as the former does not have a loader."

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
-//import vietnamScenary from "../images/vietnam-overview.mp4";
-//import vietnamDish from "../images/vietnamese-dish.mp4";
+import vietnamScenary from "../images/vietnam-overview.gif";
+import vietnamDish from "../images/vietnamese-dish.gif";
 import noodleSoup from "../images/pho-ga-noodle-chicken-soup.png";
 import vietnamLove from "../images/we-love-vietnam.png";
 
@@ -7,7 +7,8 @@ import vietnamLove from "../images/we-love-vietnam.png";
 const slidesObject = {};
 
 class ImageSlide {
-  constructor(altText, media, index) {
+  constructor(name, altText, media, index) {
+    this.name = name;
     this.altText = altText;
     this.media = media;
     this.index = index;
@@ -42,22 +43,43 @@ const homeGenerator = {
     return carrousel;
   },
   createSlides: function (carrousel) {
+    const firstGif = new ImageSlide(
+      "vietnam-overview",
+      "A sky scene overlooking the terrains of Vietnam. The beach, the mountains, and the environment is clearly visible",
+      vietnamScenary,
+      0
+    );
+    const secondGif = new ImageSlide(
+      "vietnam-dish",
+      "A shot of a delicious traditional Vietnamese dish.",
+      vietnamDish,
+      1
+    );
     const firstImage = new ImageSlide(
+      "noodle-soup",
       "Pho Ga Noodle Chicken Soup, chopsticks, and a white background",
       noodleSoup,
       2
     );
     const secondImage = new ImageSlide(
+      "vietnam-love",
       "We love Vietnam. Map of Vietnam with a red and yellow color scheme",
       vietnamLove,
       3
     );
+    slidesObject.firstGif = firstGif;
+    slidesObject.secondGif = secondGif;
     slidesObject.firstImage = firstImage;
     slidesObject.secondImage = secondImage;
-    this.appendMedia(carrousel);
+    this.processMedia(carrousel);
   },
-  appendMedia: function (carrousel) {
-    console.log(carrousel);
+  processMedia: function (carrousel) {
+    Object.keys(slidesObject).forEach((key) => {
+      this.appendMedia(carrousel, slidesObject[key]);
+    });
+  },
+  appendMedia: function (carrousel, obj) {
+    console.log(obj);
   },
 };
 
